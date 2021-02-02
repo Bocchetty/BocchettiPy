87188ef831b33968f31fceef7730fa76a802817b
Bocchetty <bocchettifrancesco06@outlook.it>
2021-02-02 20:46:57 +0100

    Riorganizazzione repo

---
+++ Interfaccia/Esecutore di file V4.py
@@ -0,0 +1,27 @@
+from os import chdir, remove, startfile
+from guizero import *
+from random import *
+import matplotlib.pyplot as plt
+
+chdir ('BocchettiPy')
+app = App(title="Esecutore di file", width=600, height=600, bg="#f4d742")
+
+file = Text(app, text="Inserisci il nome del file:")
+filename = TextBox (app, width=50)
+
+def tasto1():
+    output= f= filename.value
+    output= startfile(f)
+
+
+def tasto2():
+    output=grafico1= Picture(app, image= "grafico.png")
+
+p1 = PushButton(app,text='Esegui file',command=tasto1)
+
+p2 = PushButton(app,text='Mostra Grafico',command=tasto2)
+
+p1.bg = "#41f465"
+p2.bg = "#41f465"
+
+app.display()
