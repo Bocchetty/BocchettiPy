0f49c8b8379224d2217a69ecc715c3200248dffb
Bocchetty <bocchettifrancesco06@outlook.it>
2021-05-04 22:12:28 +0200

    Update Generatore di Haiku.py

--- Haiku/Generatore di Haiku.py
+++ Haiku/Generatore di Haiku.py
@@ -1,7 +1,20 @@
 import os
 import random
+import tkinter
+
 os.chdir('BocchettiPy\Haiku')
 
 with open('Versi da 5.txt') as f1, open('Versi da 7.txt') as f2:
     for line1, line2, line3 in zip(f1, f2):
         print(line1+line2+line3)
+
+def istruzioni():
+ messagebox.showinfo('istruzioni','cliccare il bottone in basso per generare automaticamente l Haiku?')
+root=Tk()
+root.geometry('600x600')
+root.title("GENERATORE DI HAIKU AUTOMATICO")
+lbl=Label(root,text="Benvenuto nel generatore automatico di Haiku!", font=(20))
+lbl.pack()
+b=Button(root, text="Cliccami per le istruzioni d'uso", command=istruzioni)
+b.pack()
+root.mainloop()
